fe12efdda09197dfa21313c5727452a463becc84
DanielM-IT <7ebe7@gmail.com>
2019-08-11 22:38:27 +1200

    First 3 storyboard images created.

---
+++ Planet X/CreateAccount.Designer.cs
@@ -0,0 +1,199 @@
+﻿namespace Planet_X
+{
+    partial class CreateAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateAccount));
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnCreateGame = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblRegistration = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DimGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(107, 312);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 25);
+            this.button1.TabIndex = 40;
+            this.button1.Text = "Create Account";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // btnCreateGame
+            // 
+            this.btnCreateGame.BackColor = System.Drawing.Color.DimGray;
+            this.btnCreateGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreateGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateGame.ForeColor = System.Drawing.Color.Black;
+            this.btnCreateGame.Location = new System.Drawing.Point(586, 370);
+            this.btnCreateGame.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCreateGame.Name = "btnCreateGame";
+            this.btnCreateGame.Size = new System.Drawing.Size(88, 23);
+            this.btnCreateGame.TabIndex = 39;
+            this.btnCreateGame.Text = "Return";
+            this.btnCreateGame.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Maroon;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Location = new System.Drawing.Point(652, 12);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(22, 20);
+            this.btnClose.TabIndex = 38;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // lblRegistration
+            // 
+            this.lblRegistration.AutoSize = true;
+            this.lblRegistration.BackColor = System.Drawing.Color.Transparent;
+            this.lblRegistration.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lblRegistration.Font = new System.Drawing.Font("Rockwell Extra Bold", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRegistration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblRegistration.Location = new System.Drawing.Point(78, 57);
+            this.lblRegistration.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblRegistration.Name = "lblRegistration";
+            this.lblRegistration.Size = new System.Drawing.Size(251, 35);
+            this.lblRegistration.TabIndex = 37;
+            this.lblRegistration.Text = "New Account";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(84, 262);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(193, 20);
+            this.txtPassword.TabIndex = 36;
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(84, 210);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(193, 20);
+            this.txtUserName.TabIndex = 35;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(84, 159);
+            this.txtEmail.Margin = new System.Windows.Forms.Padding(2);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(193, 20);
+            this.txtEmail.TabIndex = 34;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblPassword.Location = new System.Drawing.Point(81, 242);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(83, 18);
+            this.lblPassword.TabIndex = 33;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblUserName.Location = new System.Drawing.Point(81, 190);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(90, 18);
+            this.lblUserName.TabIndex = 32;
+            this.lblUserName.Text = "User name";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.BackColor = System.Drawing.Color.Transparent;
+            this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEmail.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblEmail.Location = new System.Drawing.Point(81, 139);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(50, 18);
+            this.lblEmail.TabIndex = 31;
+            this.lblEmail.Text = "Email";
+            // 
+            // CreateAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(685, 403);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnCreateGame);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.lblRegistration);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.lblEmail);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CreateAccount";
+            this.Text = "CreateAccount";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnCreateGame;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblRegistration;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.Label lblEmail;
+    }
+}
---
+++ Planet X/Login.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Planet_X
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.btnCreateGame = new System.Windows.Forms.Button();
+            this.btnJoinGame = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.txtUserPassword = new System.Windows.Forms.TextBox();
+            this.lblIncorrectDetails = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblUserName = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.lblLudo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCreateGame
+            // 
+            this.btnCreateGame.BackColor = System.Drawing.Color.DimGray;
+            this.btnCreateGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCreateGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCreateGame.ForeColor = System.Drawing.Color.Black;
+            this.btnCreateGame.Location = new System.Drawing.Point(503, 339);
+            this.btnCreateGame.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCreateGame.Name = "btnCreateGame";
+            this.btnCreateGame.Size = new System.Drawing.Size(88, 23);
+            this.btnCreateGame.TabIndex = 37;
+            this.btnCreateGame.Text = "Return";
+            this.btnCreateGame.UseVisualStyleBackColor = false;
+            // 
+            // btnJoinGame
+            // 
+            this.btnJoinGame.BackColor = System.Drawing.Color.DimGray;
+            this.btnJoinGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnJoinGame.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJoinGame.ForeColor = System.Drawing.Color.Black;
+            this.btnJoinGame.Location = new System.Drawing.Point(101, 268);
+            this.btnJoinGame.Margin = new System.Windows.Forms.Padding(2);
+            this.btnJoinGame.Name = "btnJoinGame";
+            this.btnJoinGame.Size = new System.Drawing.Size(97, 25);
+            this.btnJoinGame.TabIndex = 36;
+            this.btnJoinGame.Text = "Login";
+            this.btnJoinGame.UseVisualStyleBackColor = false;
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.Maroon;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnClose.Location = new System.Drawing.Point(569, 11);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(22, 20);
+            this.btnClose.TabIndex = 35;
+            this.btnClose.Text = "X";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // txtUserPassword
+            // 
+            this.txtUserPassword.Location = new System.Drawing.Point(76, 194);
+            this.txtUserPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUserPassword.Name = "txtUserPassword";
+            this.txtUserPassword.PasswordChar = '*';
+            this.txtUserPassword.Size = new System.Drawing.Size(172, 20);
+            this.txtUserPassword.TabIndex = 34;
+            // 
+            // lblIncorrectDetails
+            // 
+            this.lblIncorrectDetails.AutoSize = true;
+            this.lblIncorrectDetails.BackColor = System.Drawing.Color.Transparent;
+            this.lblIncorrectDetails.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIncorrectDetails.ForeColor = System.Drawing.Color.Gold;
+            this.lblIncorrectDetails.Location = new System.Drawing.Point(73, 223);
+            this.lblIncorrectDetails.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblIncorrectDetails.Name = "lblIncorrectDetails";
+            this.lblIncorrectDetails.Size = new System.Drawing.Size(175, 13);
+            this.lblIncorrectDetails.TabIndex = 33;
+            this.lblIncorrectDetails.Text = "User name or password is incorrect.";
+            this.lblIncorrectDetails.Visible = false;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.ForeColor = System.Drawing.Color.Red;
+            this.lblPassword.Location = new System.Drawing.Point(73, 175);
+            this.lblPassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(82, 17);
+            this.lblPassword.TabIndex = 32;
+            this.lblPassword.Text = "Password:";
+            // 
+            // lblUserName
+            // 
+            this.lblUserName.AutoSize = true;
+            this.lblUserName.BackColor = System.Drawing.Color.Transparent;
+            this.lblUserName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserName.ForeColor = System.Drawing.Color.Red;
+            this.lblUserName.Location = new System.Drawing.Point(73, 126);
+            this.lblUserName.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblUserName.Name = "lblUserName";
+            this.lblUserName.Size = new System.Drawing.Size(93, 17);
+            this.lblUserName.TabIndex = 31;
+            this.lblUserName.Text = "User Name:";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(76, 145);
+            this.txtUserName.Margin = new System.Windows.Forms.Padding(2);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(172, 20);
+            this.txtUserName.TabIndex = 30;
+            // 
+            // lblLudo
+            // 
+            this.lblLudo.AutoSize = true;
+            this.lblLudo.BackColor = System.Drawing.Color.Transparent;
+            this.lblLudo.Font = new System.Drawing.Font("Rockwell Extra Bold", 22F, System.Drawing.FontStyle.Bold);
+            this.lblLudo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.lblLudo.Location = new System.Drawing.Point(95, 44);
+            this.lblLudo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblLudo.Name = "lblLudo";
+            this.lblLudo.Size = new System.Drawing.Size(119, 35);
+            this.lblLudo.TabIndex = 29;
+            this.lblLudo.Text = "Login";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(607, 373);
+            this.Controls.Add(this.btnCreateGame);
+            this.Controls.Add(this.btnJoinGame);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtUserPassword);
+            this.Controls.Add(this.lblIncorrectDetails);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUserName);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.lblLudo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateGame;
+        private System.Windows.Forms.Button btnJoinGame;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox txtUserPassword;
+        private System.Windows.Forms.Label lblIncorrectDetails;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblUserName;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lblLudo;
+    }
+}
